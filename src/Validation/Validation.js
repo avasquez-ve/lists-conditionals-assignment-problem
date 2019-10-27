eda64ae6cce6bc0ffe6467cd72b8295f3dead751
avasquez-ve <arnellvc@gmail.com>
2019-10-26 21:26:52 -0300

    Espacio, solo test

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Validation = (props) => {
     const {strLength} = props;
+    
     let validationOutput = strLength < 7 ? "Your PJ name is too short" : "PJ name long enough";
 
     return(
